d0b7fa833cc6aa2a490d33df2a5bcf3309aaff47
Keshav Rao H <56464446+KeshavRao19@users.noreply.github.com>
2024-09-12 12:25:24 -0400

    Update mac_changer.py
    Added more functionality and ease of use.

--- mac_changer.py
+++ mac_changer.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
 
+#Run the program using python3
+#Use --help to see all the parsing options for ease of use
+
 import subprocess
+import optparse
 
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Defines the interface to change its MAC address.")
+    parser.add_option("-m", "--mac", dest="new_mac", help="Defines the new MAC address that is going to be changed on respective interface")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("Please specify a new MAC, use --help for more info.")
+    return options
+
+def change_mac(interface, new_mac):
+    print("Changing the MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig",interface,"down"])
+    subprocess.call(["ifconfig",interface,"hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface,"up"])
 
 
-#if you want to change the wired lan settings then you can use this - 
-
-# subprocess.call("ifconfig wlan0 down", shell=True)
-# subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
-# subprocess.call("ifconfig wlan0 up", shell=True)
-
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
